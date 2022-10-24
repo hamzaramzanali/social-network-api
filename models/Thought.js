e4cffb161cccf7d15e2dad5d1112511727ac9608
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-10-24 18:34:26 -0500

    added controllers, models and added userRoutes

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `responses` that gets the amount of response per video
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Video model
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
